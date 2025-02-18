cee2cafcc1f77ddcf2063ae29d52e1818ad6d040
Flex-xD <bishtaman88102@gmail.com>
2025-02-18 14:50:00 +0530

    Fixed all the error in the Authentication , now onto the styling of the auth page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express , {Request , Response} from "express";
 import dotenv from "dotenv";
 // import jikan from "jikanjs";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 import authRoutes from "./routes/authRoutes.js"
 import {connectDB} from "./config/db.js";
 dotenv.config();
@@ -11,8 +12,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use("/api/auth"  , authRoutes);
+app.use(cors({
+    origin:"http://localhost:5173" , 
+    credentials:true
+}))
+
 app.use(cookieParser());
+app.use("/api/auth"  , authRoutes);
 
 app.listen(port , () => {
     console.log(`Server running on PORT : ${port}`);
--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,8 +1,8 @@
 export const HOST = "http://localhost:5000";
 
-export const AUTH_ROUTES="/api/auth"
-export const REGISTER_ROUTES=`${AUTH_ROUTES}/register`
-export const LOGIN_ROUTES=`${AUTH_ROUTES}/login`
+const AUTH_ROUTES="/api/auth"
+export const REGISTER_ROUTES=`${HOST}${AUTH_ROUTES}/register`
+export const LOGIN_ROUTES=`${HOST}${AUTH_ROUTES}/login`
 export const GET_USER_INFO = `${AUTH_ROUTES}/user-info`
 export const UPDATE_PROFILE_ROUTE = `${AUTH_ROUTES}/update-profile`
 export const ADD_PROFILE_IMAGE_ROUTE = `${AUTH_ROUTES}/add-profile-image`
