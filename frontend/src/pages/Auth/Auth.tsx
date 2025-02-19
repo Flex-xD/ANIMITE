57fa06fb5a2b9b6aec1f6242c901f4fd97fc7705
Flex-xD <bishtaman88102@gmail.com>
2025-02-19 15:42:59 +0530

    More code to Auth.tsx

--- frontend/src/pages/Auth/Auth.tsx
+++ frontend/src/pages/Auth/Auth.tsx
@@ -55,15 +55,22 @@ function Auth() {
         }
     }
 
-    const handleChange = (setter:React.Dispatch<React.SetStateAction<string>>) => (
-        e:React.ChangeEvent<HTMLInputElement>
+    const handleChange = (setter: React.Dispatch<React.SetStateAction<string>>) => (
+        e: React.ChangeEvent<HTMLInputElement>
     ) => setter(e.target.value);
 
     return (
-        <div className="h-[100vh] w-[100vw] flex justify-center items-center bg-gray-300">
+        <div className="h-[100vh] w-[100vw] flex justify-center items-center">
 
-            <div className="h-[100vh] w-[50vw] flex justify-center items-center bg-gradient-to-r from-blue-200 to-green-200">
-                <div className="w-[64%] h-[60%] bg-gradient-to-t from-cyan-100 to-blue-100 rounded-lg flex flex-col justify-center items-center shadow-2xl">
+            <div className="h-[100vh] w-[50vw] flex flex-col justify-center items-center bg-gradient-to-r from-blue-200 via-green-200 to-cyan-200 bg-animated-gradient">
+                <div className="h-[20vh] w-[90%] flex flex-col items-center justify-center">
+                    <h1 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-cyan-600 font-[Poppins]">REGISTER AND BECOME ONE 
+                        OF US !</h1>
+                        <p className="text-center text-balance font-medium text-gray-700 text-sm">
+                            Join our community and explore the world of anime with us. Sign up today and start your journey!
+                        </p>
+                </div>
+                <div className="w-[64%] h-[60%] bg-gradient-to-r from-cyan-200 via-green-200 to-blue-200 bg-animated-gradient rounded-lg flex flex-col justify-center items-center shadow-2xl">
                     <Tabs className="w-3/4" defaultValue="login">
                         <TabsList className="bg-transparent rounded-none w-full">
                             <TabsTrigger value="login" className="data-[state=active]:bg-blue text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-blue-500 p-3 transition-all duration-300 shadow-xl">Login</TabsTrigger>
@@ -121,8 +128,22 @@ function Auth() {
                 </div>
             </div>
 
-            <div className="h-[100vh] w-[50vw] bg-gradient-to-r from-cyan-300 to-green-300">
-
+            <div className="h-[100vh] w-[50vw] flex flex-col justify-center items-center">
+                <div className="h-full w-full border-2 border-black flex flex-col items-center justify-center bg-gradient-to-r from-red-200 via-green-100 to-purple-200 bg-animated-gradient">
+                    <div className="h-[27vh] w-[23vw] flex flex-col items-center justify-center border border-black bg-white">
+                        LOGO
+                    </div>
+                    <h1 className="text-3xl font-bold">HEADING ....</h1>
+                    <p className="text-balance text-center">Welcome to our platform! Experience seamless authentication and enjoy our services.
+                        Join us today and be part of our community.</p>
+                </div>
+                <div className="h-full w-full border-2 border-black flex items-center justify-center bg-gradient-to-r from-purple-200 via-red-100 to-green-200 bg-animated-gradient">
+                    <div className="h-[43vh] w-[46vw] border border-black">
+                        <div className="h-full w-full flex justify-center items-center p-5 text-center bg-white">
+                            Changing Images with changing Intervals and anime fact , quotes or news in between 
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
