588293bd6c9e5628d20df62eec045f6a22af1813
Flex-xD <bishtaman88102@gmail.com>
2025-03-17 23:13:33 +0530

    created the getCommunityController and few lines of code to createCommunityController , now have to check getCommunityController API (If it's working or not Obv.)

--- backend/src/controllers/communityController.ts
+++ backend/src/controllers/communityController.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { IAuthRequest } from "../middlewares/authMiddleware.js";
 import User, { IUser } from "../models/User.js";
 import Community from "../models/Community.js";
 import mongoose from "mongoose";
 import { string } from "zod";
+import { error, timeStamp } from "console";
 
 export const createCommunityController = async (req: IAuthRequest, res: Response) => {
     const session = await mongoose.startSession();
@@ -22,8 +23,13 @@ export const createCommunityController = async (req: IAuthRequest, res: Response
         if (!name || typeof name !== "string") {
             return res.status(400).json({ msg: "Community name is required !" });
         }
-
-        if (name.trim().length < 3) { 
+        const communityRegex = /^[a-z_.]+$/;
+        if(!communityRegex.test(name)) {
+            res.status(400).json({
+                msg:"Community names can only contain lowercase letters, underscores (_), and dots (.)"
+            })
+        }
+        if (name.trim().length < 3) {
             return res.status(400).json({ msg: "Community name must be at least 3 characters" });
         }
 
@@ -60,7 +66,7 @@ export const createCommunityController = async (req: IAuthRequest, res: Response
             members: [admin._id]
         }
         return res.status(201).json({
-            success:true ,
+            success: true,
             msg: "Community created !",
             community: sanitizedCommunity
         })
@@ -69,19 +75,66 @@ export const createCommunityController = async (req: IAuthRequest, res: Response
         await session.abortTransaction();
         console.log({ error });
         return res.status(500).json({
-            success:false ,
-            error: error 
+            success: false,
+            error: error
         });
     } finally {
         await session.endSession();
     }
 }
 
-export const getCommunityController = async (req:IAuthRequest , res:Response) => {
+
+// ? Logic for getting all the communities 
+// ! Improvments : Make types for the community and make it compatible with the code written 
+
+const handleError = (error: unknown): string => {
+    if (error instanceof Error) {
+        return error.message
+    }
+    return "An Internal Server error occured !"
+}
+
+interface ICommunityName {
+    communityName:string
+}
+
+export const getCommunityController = async (req: IAuthRequest, res: Response):Promise<void> => {
     try {
-        const {name} = req.body;
-        if(!name) return;
-    } catch (error) {
+        const {communityName}=  req.body as ICommunityName;
+        if (!communityName || typeof communityName !== "string" || communityName.trim() == "") {
+            res.status(400).json({
+                success:false ,
+                msg:"Please enter a valid community name !"
+            });
+            return;
+        }
         
+        const regex = new RegExp(communityName.trim() , "i");
+        const communities = await Community.find({name :{$regex:regex}})
+        .sort({timeStamp:1})
+        .limit(10)
+        .select('name timeStamp')
+        .lean()
+
+        if (!communities.length) {
+            res.status(400).json({
+                success:false ,
+                msg:`No communites found matching ${communityName}` , 
+                data:[]
+            });
+            return;
+        }
+        res.status(200).json({
+            success:true ,
+            msg:"Community successfully retrived !" , 
+            data:communities
+        })
+    } catch (error) {
+        console.log("Error in the community controller " , handleError(error));
+        res.status(500).json({
+            success:false ,
+            msg:"An Internal server error occured !" , 
+            error:handleError(error)
+        })
     }
 }
