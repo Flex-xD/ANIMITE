91b916a9c401d3b60189945a05f81b75f8ce75ad
Flex-xD <bishtaman88102@gmail.com>
2025-04-16 17:33:59 +0530

    Created the layout for the explore section and will make it's component

--- frontend/src/pages/Home/components/ExploreSection/index.tsx
+++ frontend/src/pages/Home/components/ExploreSection/index.tsx
@@ -7,18 +7,32 @@ function ExploreSection({ }: Props) {
         <div className="h-[130vh] w-[100vw] bg-gradient-to-r from-[#7733EC] to-[rgba(142,0,250,0.7)] flex flex-col items-center justify-evenly">
 
             {/* FIRST MAIN DIV */}
-            <div className="flex items-center justify-evenly bg- border border-white h-[62vh] w-screen">
+            <div className="flex flex-col items-center justify-center bg- border border-white h-[62vh] w-screen p-2">
 
-                <div className="h-[61vh] w-3/6 border border-white">
+                <div className="h-[52vh] w-full border-2 bord flex items-center justify-evenly p-2">
+
+                    <div className="h-[48vh] w-4/6 border border-white p-2">
+
+                    </div>
+
+                    <div className="h-[48vh] w-3/5 border border-white flex items-center justify-center p-2">
+                        <img src={AnimiteLogo} alt="Animite Logo" className="h-72 opacity-70 " />
+                    </div>
+
                 </div>
 
-                <div className="h-[61vh] w-2/5 border border-white flex items-center justify-center">
-                    <img src={AnimiteLogo} alt="Animite Logo" className="h-72 opacity-85 "/>
+                <div className="h-[9vh] w-full p-2 flex items-center justify-center">
+                    <div className="h-[8vh] w-[20vw] border border-white">
+                        I will have buttons here !
+                    </div>
                 </div>
+
             </div>
 
             {/* SECOND MAIN DIV */}
-            <div className="flex items-center justify-evenly border border-white h-[62vh] w-screen"></div>
+            <div className="flex items-center justify-evenly border border-white h-[62vh] w-screen bg-pink-400 opacity-50">
+
+            </div>
         </div>
     )
 }
